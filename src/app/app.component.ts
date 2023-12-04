f6602f5fcc94389fe2bf352fa28b8e5931197205
pernillepersson <pernillekp@me.com>
2023-12-04 14:59:55 +0100

    Navigation bar implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,6 +9,33 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'CoffeeShopWeb';
+
+  ngOnInit() {
+    this.navigate('comp5', 'btn5');
+  }
+
+  navigate(divId: string, btnId: string) {
+    //Set alle elemented til ikke active og ikke show content
+    //Derefter sæt id-et til active og show
+    let components = document.getElementsByClassName("component");
+    // @ts-ignore
+    for (let div of components) {
+      // @ts-ignore
+      div.style.display = 'none';
+    }
+    let divs = document.getElementsByTagName("button");
+    // @ts-ignore
+    for (let div of divs){
+      div.classList.remove('active');
+    }
+    let divToShow = document.getElementById(divId);
+    let btnToActivate = document.getElementById(btnId);
+    // @ts-ignore
+    divToShow.style.display = 'block';
+    // @ts-ignore
+    btnToActivate.classList.add('active');
+
+  }
 }
