ffa9d809c66f2edda3115a8b87bb9778398a28e5
pernillepersson <pernillekp@me.com>
2023-12-05 12:48:47 +0100

    Admin sidenavbar working.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -9,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AdminComponent {
 
+  navigate(divId: string, btnId: string) {
+    //Set alle elemented til ikke active og ikke show content
+    //Derefter sæt id-et til active og show
+    let components = document.getElementsByClassName("admincomponent");
+    // @ts-ignore
+    for (let div of components) {
+      // @ts-ignore
+      div.style.display = 'none';
+    }
+    let divs = document.getElementsByClassName("adminbutton");
+    // @ts-ignore
+    for (let div of divs){
+      div.classList.remove('active');
+    }
+    let divToShow = document.getElementById(divId);
+    let btnToActivate = document.getElementById(btnId);
+    // @ts-ignore
+    divToShow.style.display = 'block';
+    // @ts-ignore
+    btnToActivate.classList.add('active');
+  }
 }
